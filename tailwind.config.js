d96f5f077a39e1b0b96de93957ece7f348bc574b
Hitarashi <sayeed205@gmail.com>
2023-08-10 10:32:00 +0530

    add hamburger menu for mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,7 +76,7 @@ module.exports = {
         },
     },
     plugins: [
-        // require('tailwindcss-animate'),
+        require('tailwindcss-animate'),
         // require('@tailwindcss/typography'),
     ],
 };
