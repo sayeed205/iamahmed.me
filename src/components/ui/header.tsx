c122180cf34451f83b5bcebdd14d4e493b494c21
Hitarashi <sayeed205@gmail.com>
2023-08-10 10:32:00 +0530

    add hamburger menu for mobile view

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,49 +1,81 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
+import { Turn as Hamburger } from 'hamburger-react';
 
 import { cn } from '@/lib/utils';
 
 import { ModeToggle } from '../mode-toggle';
 import { buttonVariants } from './button';
 
+const navItem = [
+    { name: 'Projects', href: '/projects' },
+    { name: 'Skills', href: '/skills' },
+    { name: 'Contact', href: '/contact' },
+    { name: 'Resume', href: '/resume' },
+];
+
 export default function Header() {
+    const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
     return (
         <header className="sticky top-0 z-40 h-28 bg-background">
             <div className="container flex h-24 items-center justify-between px-24 py-8">
                 <div className=""></div>
-                <div className="flex flex-row gap-2">
+
+                <nav className="hidden gap-2 md:flex md:flex-row">
                     <ModeToggle />
-                    <Link
-                        href="/projects"
-                        className={cn(
-                            buttonVariants({ variant: 'ghost', font: 'xl' })
-                        )}
-                    >
-                        Projects
-                    </Link>
-                    <Link
-                        href="/skills"
-                        className={cn(
-                            buttonVariants({ variant: 'ghost', font: 'xl' })
-                        )}
-                    >
-                        Skills
-                    </Link>
-                    <Link
-                        href="/contact"
-                        className={cn(
-                            buttonVariants({ variant: 'ghost', font: 'xl' })
-                        )}
-                    >
-                        Contact
-                    </Link>
-                    <Link
-                        href="/resume"
-                        className={cn(
-                            buttonVariants({ variant: 'ghost', font: 'xl' })
-                        )}
-                    >
-                        Resume
-                    </Link>
+                    <div className="flex flex-row gap-2">
+                        {navItem.map((item) => (
+                            <Link
+                                key={item.name}
+                                href={item.href}
+                                className={cn(
+                                    'hover:text-foreground/80 flex items-center transition-colors',
+                                    buttonVariants({
+                                        variant: 'ghost',
+                                        font: 'xl',
+                                    })
+                                )}
+                            >
+                                {item.name}
+                            </Link>
+                        ))}
+                    </div>
+                </nav>
+
+                <div
+                    className={`fixed inset-0 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md backdrop-blur duration-75 animate-in slide-in-from-bottom-80 md:hidden md:backdrop-blur-none ${
+                        showMobileMenu ? 'top-16' : 'top-[calc(100vh+4rem)]'
+                    }`}
+                >
+                    {navItem.map((item) => (
+                        <Link
+                            key={item.name}
+                            href={item.href}
+                            className={cn(
+                                `flex items-center transition-colors hover:bg-transparent`,
+                                buttonVariants({
+                                    variant: 'link',
+                                    font: 'xl',
+                                })
+                            )}
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+
+                <div className="flex md:hidden">
+                    <ModeToggle />
+                    <Hamburger
+                        onToggle={(toggle) => setShowMobileMenu(toggle)}
+                        size={24}
+                        rounded
+                        label="Show menu"
+                        hideOutline
+                        duration={0.4}
+                    />
                 </div>
             </div>
         </header>
