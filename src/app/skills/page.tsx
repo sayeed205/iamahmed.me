5cd9d5b3ca6f54849843ed0be0adeb87cd89768a
Hitarashi <sayeed205@gmail.com>
2023-08-24 11:17:03 +0530

    fix skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
 import SkillCard, { SkillInfo } from '@/components/ui/skill-card';
 import { SkillsHeading } from '@/components/heading';
 
@@ -10,18 +11,20 @@ const Skills = async () => {
         <main className="mb-7 h-[calc(100vh-10rem)] overflow-auto pt-5 text-center">
             <SkillsHeading />
 
-            <section className="flex justify-center">
-                <div className="grid-rows-8 lg:grid-col-3 relative grid gap-2 sm:mx-14 sm:mt-12 sm:grid-cols-2 sm:grid-rows-5 lg:grid-flow-col lg:grid-rows-3 lg:gap-5">
-                    {ProjectsData.map((props) => (
-                        <SkillCard
-                            key={props.id}
-                            title={props.title}
-                            language={props.language}
-                            id={props.id}
-                        />
-                    ))}
-                </div>
-            </section>
+            <ScrollArea className="">
+                <section className="flex items-center justify-center align-middle">
+                    <div className="relative grid grid-cols-1 gap-2 mb-10 sm:mx-14 sm:mt-12 sm:grid-cols-2 lg:grid-cols-3 lg:gap-5 z-[51]">
+                        {ProjectsData.map((props) => (
+                            <SkillCard
+                                key={props.id}
+                                title={props.title}
+                                language={props.language}
+                                id={props.id}
+                            />
+                        ))}
+                    </div>
+                </section>
+            </ScrollArea>
         </main>
     );
 };
